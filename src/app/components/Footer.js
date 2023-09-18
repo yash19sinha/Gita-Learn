3ab3bf8779c59b15a7305cf44ad6d6a8f166f6cb
Samridhi-Sinha <96681774+yash19sinha@users.noreply.github.com>
2023-09-19 01:23:59 +0530

    basic setup

---
+++ src/app/components/Footer.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+export const Footer = () => {
+  return (
+    <footer className="footer footer-center p-4 bg-base-300 text-base-content">
+  <aside>
+    <p>Copyright © 2023 - All right reserved by ACME Industries Ltd</p>
+  </aside>
+    </footer>
+  )
+}
