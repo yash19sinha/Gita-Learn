a32816882262f489d00dade52bd0c2666e6870ed
Samridhi-Sinha <96681774+yash19sinha@users.noreply.github.com>
2023-10-29 11:59:18 +0530

    footer

--- src/app/login/page.js
+++ src/app/login/page.js
@@ -2,14 +2,14 @@ import React from 'react'
 export default function Example() {
   return (
     <>
-      <div className="flex min-h-full flex-1 flex-col justify-center px-6 py-12 lg:px-8">
+      <div className="flex flex-col justify-center flex-1 min-h-full px-6 py-12 bg-white lg:px-8">
         <div className="sm:mx-auto sm:w-full sm:max-w-sm">
           <img
-            className="mx-auto h-10 w-auto"
+            className="w-auto h-10 mx-auto"
             src="https://i0.wp.com/cdn.prabhupadaworld.com/wp-content/uploads/2021/10/logo.webp?w=500&ssl=1"
             alt="Your Company"
           />
-          <h2 className="mt-10 text-center text-2xl font-bold leading-9 tracking-tight text-gray-900">
+          <h2 className="mt-10 text-2xl font-bold leading-9 tracking-tight text-center text-gray-900">
             Sign in to your account
           </h2>
         </div>
@@ -17,7 +17,7 @@ export default function Example() {
         <div className="mt-10 sm:mx-auto sm:w-full sm:max-w-sm">
           <form className="space-y-6" action="#" method="POST">
             <div>
-              <label htmlFor="email" className="block text-sm font-medium leading-6 text-gray-900">
+              <label htmlFor="email" className="block text-sm font-medium leading-6 text-gray-900 bg-white">
                 Email address
               </label>
               <div className="mt-2">
@@ -27,18 +27,18 @@ export default function Example() {
                   type="email"
                   autoComplete="email"
                   required
-                  className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-orange-600 sm:text-sm sm:leading-6"
+                  className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-orange-600 sm:text-sm sm:leading-6 bg-white"
                 />
               </div>
             </div>
 
             <div>
               <div className="flex items-center justify-between">
-                <label htmlFor="password" className="block text-sm font-medium leading-6 text-gray-900">
+                <label htmlFor="password" className="block text-sm font-medium leading-6 text-gray-900 bg-white">
                   Password
                 </label>
                 <div className="text-sm">
-                  <a href="/forgetpassword" className="font-semibold text-orange-600 hover:text-orange-500">
+                  <a href="/forgetpassword" className="font-semibold text-orange-600 bg-white hover:text-orange-500">
                     Forgot password?
                   </a>
                 </div>
@@ -50,7 +50,7 @@ export default function Example() {
                   type="password"
                   autoComplete="current-password"
                   required
-                  className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-orange-600 sm:text-sm sm:leading-6"
+                  className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-orange-600 sm:text-sm sm:leading-6 bg-white"
                 />
               </div>
             </div>
@@ -65,7 +65,7 @@ export default function Example() {
             </div>
           </form>
 
-          <p className="mt-10 text-center text-sm text-gray-500">
+          <p className="mt-10 text-sm text-center text-gray-500">
             Not a member?{' '}
             <a href="/signup.js" className="font-semibold leading-6 text-orange-600 hover:text-orange-500">
               Create an Account
