94b02136664b2486174a5763201504ed54c181f6
Samridhi-Sinha <96681774+yash19sinha@users.noreply.github.com>
2024-02-17 16:25:06 +0530

    search bar changes

--- src/app/components/SearchResults.js
+++ src/app/components/SearchResults.js
@@ -6,9 +6,14 @@ const SearchResults = ({ searchResults }) => {
       <ul className="p-5 m-4 mt-4">
         {searchResults.map(result => (
           <li key={result.verse_number} className="py-4 border-b border-gray-200">
-            <p className="">{result.highlighted_content}</p>
-            <p className="text-red-500">{result.user_query}</p>
-            <Link href={result.verse_link} className="text-blue-500 hover:text-blue-600">Go to verse</Link>
+            <p className="justify-between p-4 px-8 text-lg">{result.highlighted_content}</p>
+            <p className="justify-between p-4 px-8 text-lg text-red-500">{result.user_query}</p>
+            
+            <button>
+            <Link href={result.verse_link} className="px-4 py-2 ml-2 font-semibold text-white bg-blue-500 rounded-full hover:bg-blue-600 focus:outline-none">Go to verse</Link>
+            </button>
+            
+           
           </li>
         ))}
       </ul>
